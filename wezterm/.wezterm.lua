1fbfa50ca1f58543cba07addf5f0de3c11201d89
Over Martinez <overmartinez@gmail.com>
2024-11-28 08:01:59 -0300

    feat(wezterm): Change font-family and font-size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,9 +2,10 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Geist Mono")
--- config.font = wezterm.font("JetBrains Mono")
-config.font_size = 19
+-- config.font = wezterm.font("MesloLGS NF")
+-- config.font = wezterm.font("Geist Mono")
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 17
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
