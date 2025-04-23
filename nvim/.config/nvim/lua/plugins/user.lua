f83b584805a04dc04e5a0bcf793f70051f868ee2
Over Martinez <overmartinez@gmail.com>
2025-04-23 19:10:11 -0400

    nvim: minor changes

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -5,85 +5,85 @@
 
 ---@type LazySpec
 return {
-  {
-    "AndreM222/copilot-lualine",
-    config = function()
-      local lualine = require "lualine"
-
-      lualine.setup {
-        options = {
-          icons_enabled = true,
-          -- theme = "palenight",
-          theme = "horizon",
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = {},
-          always_divide_middle = true,
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = {
-            "branch",
-            "diff",
-            {
-              "diagnostics",
-              sources = { "nvim_diagnostic" },
-              symbols = { error = " ", warn = " ", info = " ", hint = " " },
-            },
-          },
-          lualine_c = { "selectioncount" },
-          lualine_x = {
-            {
-              "copilot",
-              -- Default values
-              symbols = {
-                status = {
-                  icons = {
-                    enabled = " ",
-                    sleep = " ", -- auto-trigger disabled
-                    disabled = " ",
-                    warning = " ",
-                    unknown = " ",
-                  },
-                  hl = {
-                    enabled = "#50FA7B",
-                    sleep = "#AEB7D0",
-                    disabled = "#6272A4",
-                    warning = "#FFB86C",
-                    unknown = "#FF5555",
-                  },
-                },
-                spinners = require("copilot-lualine.spinners").dots,
-                spinner_color = "#6272A4",
-              },
-              show_colors = false,
-              show_loading = true,
-            },
-            "searchcount",
-            "fileformat",
-            "filetype",
-          },
-          lualine_y = { "progress" },
-          lualine_z = { "buffers" },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
-        },
-        tabline = {},
-        extensions = {},
-      }
-    end,
-  },
-  {
-    "echasnovski/mini.bufremove",
-    version = "*",
-    opts = function() require("mini.bufremove").setup() end,
-  },
+  -- {
+  --   "AndreM222/copilot-lualine",
+  --   config = function()
+  --     local lualine = require "lualine"
+  --
+  --     lualine.setup {
+  --       options = {
+  --         icons_enabled = true,
+  --         -- theme = "palenight",
+  --         theme = "horizon",
+  --         component_separators = { left = "", right = "" },
+  --         section_separators = { left = "", right = "" },
+  --         disabled_filetypes = {},
+  --         always_divide_middle = true,
+  --       },
+  --       sections = {
+  --         lualine_a = { "mode" },
+  --         lualine_b = {
+  --           "branch",
+  --           "diff",
+  --           {
+  --             "diagnostics",
+  --             sources = { "nvim_diagnostic" },
+  --             symbols = { error = " ", warn = " ", info = " ", hint = " " },
+  --           },
+  --         },
+  --         lualine_c = { "selectioncount" },
+  --         lualine_x = {
+  --           {
+  --             "copilot",
+  --             -- Default values
+  --             symbols = {
+  --               status = {
+  --                 icons = {
+  --                   enabled = " ",
+  --                   sleep = " ", -- auto-trigger disabled
+  --                   disabled = " ",
+  --                   warning = " ",
+  --                   unknown = " ",
+  --                 },
+  --                 hl = {
+  --                   enabled = "#50FA7B",
+  --                   sleep = "#AEB7D0",
+  --                   disabled = "#6272A4",
+  --                   warning = "#FFB86C",
+  --                   unknown = "#FF5555",
+  --                 },
+  --               },
+  --               spinners = require("copilot-lualine.spinners").dots,
+  --               spinner_color = "#6272A4",
+  --             },
+  --             show_colors = false,
+  --             show_loading = true,
+  --           },
+  --           "searchcount",
+  --           "fileformat",
+  --           "filetype",
+  --         },
+  --         lualine_y = { "progress" },
+  --         lualine_z = { "buffers" },
+  --       },
+  --       inactive_sections = {
+  --         lualine_a = {},
+  --         lualine_b = {},
+  --         lualine_c = { "filename" },
+  --         lualine_x = { "location" },
+  --         lualine_y = {},
+  --         lualine_z = {},
+  --       },
+  --       tabline = {},
+  --       extensions = {},
+  --     }
+  --   end,
+  -- },
+  -- {
+  --   "echasnovski/mini.bufremove",
+  --   version = "*",
+  --   opts = function() require("mini.bufremove").setup() end,
+  -- },
   {
     "kylechui/nvim-surround",
     event = { "BufReadPre", "BufNewFile" },
@@ -132,16 +132,16 @@ return {
   -- },
 
   -- -- You can disable default plugins as follows:
-  {
-    "rebelot/heirline.nvim",
-    config = function()
-      require("heirline").setup {
-        tabline = {
-          enabled = false,
-        },
-      }
-    end,
-  },
+  -- {
+  --   "rebelot/heirline.nvim",
+  --   config = function()
+  --     require("heirline").setup {
+  --       tabline = {
+  --         enabled = false,
+  --       },
+  --     }
+  --   end,
+  -- },
   --
   -- -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
