3e636e378ded5f7ce77c58435da386b0e89b3151
Over Martinez <overmartinez@gmail.com>
2025-04-24 22:15:25 -0400

    Create codeium.lua file
    It is needed to run `:Codeium Auth` to authenticate and get the token

---
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,45 @@
+return {
+  "Exafunction/windsurf.vim",
+  cmd = {
+    "Codeium",
+    "CodeiumEnable",
+    "CodeiumDisable",
+    "CodeiumToggle",
+    "CodeiumAuto",
+    "CodeiumManual",
+  },
+  event = "BufEnter",
+  dependencies = {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>;"] = {
+            "<Cmd>CodeiumToggle<CR>",
+            noremap = true,
+            desc = "Toggle Codeium active",
+          },
+        },
+        i = {
+          ["<C-g>"] = {
+            function() return vim.fn["codeium#Accept"]() end,
+            expr = true,
+          },
+          ["<C-f>"] = {
+            function() return vim.fn["codeium#CycleCompletions"](1) end,
+            expr = true,
+          },
+          ["<C-d>"] = {
+            function() return vim.fn["codeium#CycleCompletions"](-1) end,
+            expr = true,
+          },
+          ["<C-x>"] = {
+            function() return vim.fn["codeium#Clear"]() end,
+            expr = true,
+          },
+        },
+      },
+    },
+  },
+}
